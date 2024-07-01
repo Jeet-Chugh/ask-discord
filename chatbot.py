9677cb121a6ca712063a2f66711063ce4b30aea5
Jeet-Chugh <sunjeetchugh@gmail.com>
2024-06-30 21:47:27 -0500

    changed formatting and created chatbot

--- chatbot.py
+++ chatbot.py
@@ -1,12 +1,77 @@
-from openai import OpenAI
-from pymilvus import (
-    connections,
-    utility,
-    Collection
-)
+from pymilvus import Collection
+import numpy as np
+import pandas as pd
+import re
 
-class Chatbot():
+from openai import OpenAI
+import os
+
+
+class Chatbot:
 
     def __init__(self, configs) -> None:
+        self.client = configs["OPENAI_CLIENT"]
         self.MAX_SIMILAR_EXAMPLES = configs.get("MAX_SIMILAR_EXAMPLES", 10)
-        self.SIMILARITY_SCORE_CUTOFF = configs.get("SIMILARITY_SCORE_CUTOFF", 0.80)
+        self.SIMILARITY_SCORE_CUTOFF = configs.get("SIMILARITY_SCORE_CUTOFF", 0.20)
+        self.CHAT_MODEL = configs.get("CHAT_MODEL", "gpt-4o")
+        self.COLLECTION_NAME = configs.get("COLLECTION_NAME", "channel")
+        self.EMBEDDING_MODEL = configs.get("EMBEDDING_MODEL", "text-embedding-3-small")
+        self.EMBEDDING_DIMENSIONS = configs.get("EMBEDDING_DIMENSIONS", 512)
+
+        self.collection = Collection(self.COLLECTION_NAME)
+        self.collection.load()
+
+        self.search_params = {"metric_type": "COSINE", "params": {"nprobe": 8}}
+
+    def ask(self, query):
+        results = self.find_similar_messages(query)
+        prompt = f"""
+            Construct a helpful response to the following query:"{query}"
+            To form your response, only use the following texts (comma delimited): "{",".join([row["content"] for row in results])}"
+            Do not use ANY external information when crafting your response, stick strictly to the text provided.
+                  """
+        chat = self.client.chat.completions.create(
+            messages=[{"role": "user", "content": prompt}], model=self.CHAT_MODEL
+        )
+        return chat.choices[0].message.content
+
+    def find(self, query):
+        results = self.find_similar_messages(query)
+        res = []
+        for result in results:
+            res.append([
+            f"Similarity: {result['distance'] * 100:.2f}%",
+            f"Timestamp: {result['timestamp']}",
+            f"Author: {result['authorName']}",
+            f"Message: {result['content']}",
+            f"ID: {result['id']}"
+        ])
+        return res
+
+    def find_similar_messages(self, query):
+        embed_response = self.client.embeddings.create(
+            input=query,
+            model=self.EMBEDDING_MODEL,
+            dimensions=self.EMBEDDING_DIMENSIONS,
+        )
+        query_vector = np.array(embed_response.data[0].embedding)
+        output_fields = ["id", "timestamp", "content", "authorId", "authorName"]
+        top_k = self.collection.search(
+            [query_vector],
+            "embedding",
+            self.search_params,
+            limit=self.MAX_SIMILAR_EXAMPLES,
+            output_fields=output_fields,
+        )
+        results = []
+        for row in top_k[0]:
+            results.append(
+                {
+                    "distance": row.distance,
+                    "id": row.id,
+                    "content": row.content,
+                    "timestamp": row.timestamp,
+                    "authorName": row.authorName,
+                }
+            )
+        return results
--- load_data.py
+++ load_data.py
@@ -8,17 +8,21 @@ import tiktoken
 from pymilvus import (
     connections,
     utility,
-    FieldSchema, CollectionSchema, DataType,
-    Collection
+    FieldSchema,
+    CollectionSchema,
+    DataType,
+    Collection,
 )
 
 from dotenv import load_dotenv
+
 load_dotenv()
 
-class LoadData():
+
+class LoadData:
 
     def __init__(self, configs: dict) -> None:
-        self.client = OpenAI(api_key=os.getenv("OPENAI_KEY"))
+        self.client = configs["OPENAI_CLIENT"]
         self.JSON_DATA_PATH = configs.get("JSON_DATA_PATH", "/data/data.json")
         self.EMBEDDING_MODEL = configs.get("EMBEDDING_MODEL", "text-embedding-3-small")
         self.EMBEDDING_DIMENSIONS = configs.get("EMBEDDING_DIMENSIONS", 512)
@@ -26,7 +30,7 @@ class LoadData():
         self.MIN_MESSAGE_LENGTH = configs.get("MIN_MESSAGE_LENGTH", 50)
         self.COLLECTION_NAME = configs.get("COLLECTION_NAME", "channel")
         self.collection = None
-        
+
         self.load_data()
 
     def load_data(self) -> None:
@@ -40,29 +44,40 @@ class LoadData():
         return self.COLLECTION_NAME in utility.list_collections()
 
     def load_json(self) -> pd.DataFrame:
-        with open(self.JSON_DATA_PATH, encoding='utf8') as data:
+        with open(self.JSON_DATA_PATH, encoding="utf8") as data:
             json_data = json.load(data)
         messages = json_data["messages"]
         df = pd.json_normalize(messages)
 
-        df = df.loc[(df["content"] != "") & (df["content"].str.len() > self.MIN_MESSAGE_LENGTH) & (df["content"].str.len() < self.MAX_MESSAGE_LENGTH) & ~(df["content"].str.startswith("http"))]
-        df = df.rename(columns={"author.id" : "authorId", "author.name" : "authorName"})
+        df = df.loc[
+            (df["content"] != "")
+            & (df["content"].str.len() > self.MIN_MESSAGE_LENGTH)
+            & (df["content"].str.len() < self.MAX_MESSAGE_LENGTH)
+            & ~(df["content"].str.startswith("http"))
+        ]
+        df = df.rename(columns={"author.id": "authorId", "author.name": "authorName"})
         RELEVANT_COLUMNS = ["id", "timestamp", "content", "authorId", "authorName"]
         return df[RELEVANT_COLUMNS]
-    
+
     @staticmethod
     def truncate_text(text) -> str:
-        tokenizer = tiktoken.get_encoding('cl100k_base')
+        tokenizer = tiktoken.get_encoding("cl100k_base")
         tokens = tokenizer.encode(text)
         if len(tokens) > 8192:
             tokens = tokens[:8192]
         return tokenizer.decode(tokens)
-    
+
     def insert_data(self, df: pd.DataFrame) -> None:
-        slices = [df[i:i+1000] for i in range(0, len(df), 1000)]
+        slices = [df[i : i + 1000] for i in range(0, len(df), 1000)]
         for s in slices:
-            res = self.client.embeddings.create(input=s["content"], model=self.EMBEDDING_MODEL, dimensions=self.EMBEDDING_DIMENSIONS)
-            s["embedding"] = [np.array(embedding.embedding, dtype="float32") for embedding in res.data]
+            res = self.client.embeddings.create(
+                input=s["content"],
+                model=self.EMBEDDING_MODEL,
+                dimensions=self.EMBEDDING_DIMENSIONS,
+            )
+            s["embedding"] = [
+                np.array(embedding.embedding, dtype="float32") for embedding in res.data
+            ]
             data = s.to_dict(orient="records")
             self.collection.insert(data)
         self.collection.flush()
@@ -86,21 +101,37 @@ class LoadData():
         # |6| "authorName"| VarChar    |                  |  "message author name"       |
         # +-+-------------+------------+------------------+------------------------------+
         fields = [
-            FieldSchema(name="id", dtype=DataType.VARCHAR, is_primary=True, auto_id=False, max_length=50),
+            FieldSchema(
+                name="id",
+                dtype=DataType.VARCHAR,
+                is_primary=True,
+                auto_id=False,
+                max_length=50,
+            ),
             FieldSchema(name="timestamp", dtype=DataType.VARCHAR, max_length=50),
-            FieldSchema(name="content", dtype=DataType.VARCHAR, max_length=self.MAX_MESSAGE_LENGTH),
-            FieldSchema(name="embedding", dtype=DataType.FLOAT_VECTOR, dim=self.EMBEDDING_DIMENSIONS),
+            FieldSchema(
+                name="content",
+                dtype=DataType.VARCHAR,
+                max_length=self.MAX_MESSAGE_LENGTH,
+            ),
+            FieldSchema(
+                name="embedding",
+                dtype=DataType.FLOAT_VECTOR,
+                dim=self.EMBEDDING_DIMENSIONS,
+            ),
             FieldSchema(name="authorId", dtype=DataType.VARCHAR, max_length=50),
-            FieldSchema(name="authorName", dtype=DataType.VARCHAR, max_length=50)
+            FieldSchema(name="authorName", dtype=DataType.VARCHAR, max_length=50),
         ]
         schema = CollectionSchema(fields)
-        self.collection = Collection(self.COLLECTION_NAME, schema, consistency_level="Strong")
+        self.collection = Collection(
+            self.COLLECTION_NAME, schema, consistency_level="Strong"
+        )
 
     def index_vectors(self) -> None:
         index = {
-                    "index_type": "IVF_FLAT",
-                    "metric_type": "COSINE",
-                    "params": {"nlist": self.EMBEDDING_DIMENSIONS},
-                }
+            "index_type": "IVF_FLAT",
+            "metric_type": "COSINE",
+            "params": {"nlist": self.EMBEDDING_DIMENSIONS},
+        }
         self.collection.create_index("embedding", index)
         self.collection.create_index(field_name="id", index_name="id_index")
